89ab0ae7052d1e1faf7aa978113c3328e0124304
yinde <yinde@hnjing.com>
2018-10-31 09:32:19 +0800

    feat(dao): 增加新功能
    增加了登录功能

--- src/main/java/com/example/DemoWebApplication.java
+++ src/main/java/com/example/DemoWebApplication.java
@@ -12,7 +12,7 @@ public class DemoWebApplication {
 
     @RequestMapping("/methodPath")
     public String greeting() {
-        return "Hello World!111111111111abc";
+        return "Hello World!111111111111abcd";
     }
     
 	public static void main(String[] args) {
